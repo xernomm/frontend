619741f8ab46ec74c223791a10fcdd59c04c7d35
xernomm <rafaelrichie03@gmail.com>
2024-06-07 11:27:23 +0700

    first commit

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../style/InputBox.css'
+import { Form } from 'react-bootstrap'
+
+const InputBox = () => {
+  return (
+    <div>
+      <Form>
+
+        
+
+      </Form>
+    </div>
+  )
+}
+
+export default InputBox
